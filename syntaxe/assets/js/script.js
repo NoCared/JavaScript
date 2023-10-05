156e48b46e4738c145b45baf3333782ca88f1583
nocared <paul.richarddu30@gmail.com>
2023-10-05 09:59:33 +0200

    Mise en place du JS

---
+++ syntaxe/assets/js/script.js
@@ -0,0 +1,14 @@
+// commentaire comme en c++/ c#
+
+
+//A ne pas faire
+var maVariable = "Chaine de caractere";
+var maVariable = 8;
+//corrigé après 2015 (ES6)
+let maVariableLet = "Chaine de caractere";
+maVariableLet = 1;
+const maVariableConst = "Chaine de caractere";
+//maVariableConst = 5; provoque une erreur
+
+
+const colorArray = ["blue", "red", "green"];
