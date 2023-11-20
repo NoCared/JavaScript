8b67f0310c9ce9b79f7408fbd43ddb2139596cfc
nocared <paul.richarddu30@gmail.com>
2023-11-20 09:03:59 +0100

    ADD - Fetch game

--- fetch/assets/js/script.js
+++ fetch/assets/js/script.js
@@ -2,42 +2,41 @@
 
 const recherche = document.querySelector("[name='recherche']");
 const retour = document.querySelector("#retour");
+let maRecherche;
+// fonction fetch ajax ASYNC
+// la fonction fetch envoie une requete à une url (son parametre)
+fetch('./assets/games/games.json')
+    // puis (then) convertit le résultat en text (.text()) ou en json (.json)
+    .then((response) => response.json())
+    // puis je peux récupérer le résultat (texte/json) pour l'exploiterdans mon javascript
+    .then((result) => {
+        maRecherche = result;
+    }).finally(() => {
+        recherche.addEventListener("keyup", () => {
 
 
-recherche.addEventListener("keyup", () => {
-    let maRecherche;
-
-    // fonction fetch ajax ASYNC
-    // la fonction fetch envoie une requete à une url (son parametre)
-    fetch('./assets/games/games.json')
-        // puis (then) convertit le résultat en text (.text()) ou en json (.json)
-        .then((response) => response.json())
-        // puis je peux récupérer le résultat (texte/json) pour l'exploiterdans mon javascript
-        .then((result) => {
-            maRecherche = result;
-        })
-        .finally(() => {
             const research = rechercheParValeur(maRecherche, "title", recherche.value);
 
             if (research != null) {
-                retour.innerHTML = 
-                `<h1>${research.title} </h1>
-                <p>- genre: ${research.genre} </p>
-                <p>- platform: ${research.platform} </p>
-                <p>- developer: ${research.developer} </p>
-                <img src="${research.thumbnail}"/>`;
+                retour.innerHTML =
+                    `<h1>${research.title} </h1>
+                        <p>- genre: ${research.genre} </p>
+                        <p>- platform: ${research.platform} </p>
+                        <p>- developer: ${research.developer} </p>
+                        <img src="${research.thumbnail}"/>`;
             }
             else {
                 retour.innerHTML = "";
             }
-        })
-});
+        });
+    })
+
+
 
 
 const rechercheParValeur = (tab, champ, value) => {
     for (const [key, valeur] of Object.entries(tab)) {
-        if (valeur[champ].includes(value))
-        {
+        if (valeur[champ].includes(value)) {
             return valeur;
         }
     }
